85c5baaf5c4624c2e4651771e682be740478e63d
lpetrus32 <louis-ep@hotmail.fr>
2026-08-20 11:17:26 +0200

    feat: add initial project setup with .gitignore, build script, and requirements

--- CadaLearningTools/settings.py
+++ CadaLearningTools/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)@-g6983o8natfuecy52sz0ij$wp*1=pa0v3s-dl)o5xe(qxma'
+# SECRET_KEY = 'django-insecure-)@-g6983o8natfuecy52sz0ij$wp*1=pa0v3s-dl)o5xe(qxma'
 
 
 SECRET_KEY = os.environ.get(
@@ -34,6 +34,7 @@ DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
+    "outils-cada.com"
 ]
 
 if os.environ.get("RENDER_EXTERNAL_HOSTNAME"):
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,8 +133,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
